b5506d950f5142f6103d18d6a0370a9677e9f6dd
Steven Kaing <skai0008@student.monash.edu>
2025-07-29 19:42:05 +1000

    feat: created colour slider with red, green and blue colours with a dynamic-view renderer that changes colour based on the rgb values

--- Workshop01/ViewController.swift
+++ Workshop01/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var colourPreviewView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        if sender == redSlider {
+            print("Red")
+        }
+        else if sender == greenSlider {
+            print("Green")
+        }
+        else if sender == blueSlider {
+            print("Blue")
+        }
+        
+        //Create the colour
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        //Change the colour background of the view
+        colourPreviewView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+            
+        
+        
+    }
+    
 }
 
