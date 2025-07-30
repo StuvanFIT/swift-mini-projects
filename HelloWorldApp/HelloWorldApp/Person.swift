43d58b829e0679228f7cbb66309c6342278e7f6c
skai0008 <kaingsteven123@gmail.com>
2025-07-30 12:07:44 +1000

    update: update helloworld app with extensions - datepicker wheel, calculating difference between currdate and birth date

--- HelloWorldApp/HelloWorldApp/Person.swift
+++ HelloWorldApp/HelloWorldApp/Person.swift
@@ -9,15 +9,61 @@ import UIKit
 
 class Person: NSObject {
     var name: String
-    var age: Int
+    var birthDate: Date
     
-    init(inputName: String, inputAge: Int) {
+    init(inputName: String, inputDate: Date) {
         name = inputName
-        age = inputAge
+        birthDate = inputDate //base: changed from age --> ext 1: dob
     }
     
     func greeting() -> String {
-        return "Hello \(name)! You are \(age) years old!"
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current //use the users device current locale regional settings
+        formatter.dateStyle = .long //styles how the date looks: .long displays the date as i.e. July 29 2025, .short 29/07/2025
+        formatter.timeStyle = .none //styles how the time looks:  no time is shown
+        
+        //This converts a Date object (like Date()) into a human-readable String, using the styles you specified.
+        let formattedDate = formatter.string(from: birthDate)
+        
+        //Calculate the difference in years, months and days
+        let timeAliveComponents = calculateDifferenceDate()
+        
+        let dayComponent = timeAliveComponents.day ?? 0
+        let monthComponent = timeAliveComponents.month ?? 0
+        let yearComponent = timeAliveComponents.year ?? 0
+        
+        let dayComponentText = dayComponent == 1 ? "1 day" : "\(dayComponent) days"
+        let monthComponentText = monthComponent == 1 ? "1 month" : "\(monthComponent) months"
+        let yearComponentText = yearComponent == 1 ? "1 year" : "\(yearComponent) years"
+        
+        let daysAlive = daysUserAlive();
+        
+        return "Hello \(name)! You were born on \(formattedDate)! You have been alive for \(yearComponentText), \(monthComponentText) and \(dayComponentText)! Or, we can say that you have been on this Earth for a total of \(daysAlive) days!"
+    }
+    
+    func calculateDifferenceDate() -> DateComponents{
+        //Calendar class has a method called: dateComponents -> returns the difference between 2 dates for each specified component
+        let calendar = Calendar.current
+        
+        //Present date
+        let currDate = Date()
+        
+        //Calculate the difference between the 2 dates: present - birth date.
+        //We will do this for days, months and years
+        let componentSet: Set<Calendar.Component> = [.year, .month, .day]
+        
+        //note that calendar.dataComponents returns an instance of the struc DataComponents, which has optional values like year, month, day, hour...
+        let timeAliveComponents = calendar.dateComponents(componentSet, from: birthDate, to: currDate)
+        print(timeAliveComponents)
+        
+        return timeAliveComponents
+    }
+    
+    func daysUserAlive () -> Int {
+        let calendar = Calendar.current
+        let currDate = Date()
+        let result = calendar.dateComponents([.day], from: birthDate, to: currDate)
+        return result.day ?? 0
     }
 
 }
--- HelloWorldApp/HelloWorldApp/ViewController.swift
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpDateField()
     }
     
     @IBAction func sayHello(_ sender: Any) {
@@ -26,7 +27,8 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var birthDateField: UITextField!
+    @IBOutlet weak var datePickerField: UIDatePicker!
     
     func displayMessage(title: String, message: String) {
         
@@ -52,25 +54,51 @@ class ViewController: UIViewController {
             return
         }
         
-        guard let ageText = ageField.text, let age = Int(ageText) else {
-            // Age could not be established. Print an error and exit
-            displayMessage(title: "Error", message: "Please enter a valid age")
+        //Extension 1/2:
+
+        guard let birthDateText = birthDateField.text, !birthDateText.isEmpty else {
+            // dob could not be established. Print an error and exit
+            displayMessage(title: "Error", message: "Please enter your date of birth in the format YYYY-MM-DD")
             return
         }
         
-        //Create an instance of Person with a name and age
-        let person = Person(inputName:name, inputAge:age)
+        //Parse the birth date text and convert to the birthDateText into a Date instance
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.locale = Locale.current
+
+        guard let birthDate = formatter.date(from: birthDateText) else {
+            displayMessage(title: "Error", message: "Date Format must be in YYYY-MM-DD")
+            return
+        }
+        
+        
+        
+//        //Extension 3:
+//        //datePickerField may be null
+//        guard let birthDate = datePickerField?.date else {
+//            displayMessage(title: "ERROR:", message: "Date picked is not found. Please contact us for help!")
+//            return
+//        }
+//       
+        
+
+        
+        //Create an instance of Person with a name and dob
+        let person = Person(inputName:name, inputDate: birthDate)
         
         //Call the greeting method and retrieve the message
         let message: String = person.greeting()
-        let title: String = "Hello, if you are reading this, please buy some NVIDEA stock!"
+        let title: String = "Hello, if you are reading this, have a great day!"
         
         //Display the message to the end-user
         displayMessage(title: title, message: message)
-        
-        
     }
     
+    func setUpDateField() {
+        //Set the placeholder
+        birthDateField.placeholder = "YYYY-MM-DD"
+    }
 
     
 
